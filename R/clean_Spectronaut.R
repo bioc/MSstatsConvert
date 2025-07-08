ddd19c67591b31a643a621ad21ddc6d17e785fd4
devonjkohler <devonjkohler@gmail.com>
2025-07-08 13:41:20 -0400

    fragment loss type

--- R/clean_Spectronaut.R
+++ R/clean_Spectronaut.R
@@ -10,7 +10,7 @@
   
   spec_input = getInputFile(msstats_object, "input")
   .validateSpectronautInput(spec_input)
-  spec_input = spec_input[FFrgLossType == "noloss", ]
+  spec_input[FFrgLossType != "noloss", paste0("F", intensity) := NA]
 
   f_charge_col = .findAvailable(c("FCharge", "FFrgZ"), colnames(spec_input))
   pg_qval_col = .findAvailable(c("PGQvalue"), colnames(spec_input))
